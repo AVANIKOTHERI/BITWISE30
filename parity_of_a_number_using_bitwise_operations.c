2f12a9633d18e82235a64bcf1be5c0b08ae1aae4
AVANIKOTHERI <aavanikotheri@gmail.com>
2025-03-18 14:41:47 +0530

    Update parity_of_a_number_using_bitwise_operations.c

--- parity_of_a_number_using_bitwise_operations.c
+++ parity_of_a_number_using_bitwise_operations.c
@@ -20,3 +20,31 @@ int main() {
     printf("Parity of %u is %s\n", num, findParity(num) ? "Odd" : "Even");
     return 0;
 }
+//---------------------------
+#include <stdio.h>
+
+// Function to check parity using num & (num - 1) and XOR
+int checkParity(int num) {
+    int parity = 0;  // Start with even parity (0)
+
+    while (num) {
+        parity ^= 1;     // Toggle parity (Odd <-> Even)
+        num &= (num - 1); // Remove the lowest set bit
+    }
+
+    return parity;  // 1 for Odd Parity, 0 for Even Parity
+}
+
+// Driver Code
+int main() {
+    int numbers[] = {7, 8, 15, 1000, 255};  // Test cases
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < n; i++) {
+        int num = numbers[i];
+        printf("Number: %d -> %s Parity\n", num, checkParity(num) ? "Odd" : "Even");
+    }
+
+    return 0;
+}
+
